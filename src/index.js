34d67beb88e194178c7b81423ac44cf494178700
vladimir <lovkiy2012@gmail.com>
2020-01-14 20:53:13 +0300

    56 api

--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
-console.log("Hellow worlrd");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-
-
-const getResource = async url => {
-  const res = await fetch(url);
-  if (!res.ok) {
-    throw new Error(`Could not fetch ${url} ` + `,received ${res.status}`);
-  }
-  const body = await res.json();
-  return body;
-};
-
-getResource("https://swapi.co/api/people/1222/")
-  .then(body => console.log(body))
-  .catch(error => {
-    console.error("Could not Fetch", error);
-  });
-
-
+ReactDOM.render(<App />,
+  document.getElementById('root'));
