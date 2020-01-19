3317c43d4b396bf6271b4c81cb7abe5d4227a122
vladimir <lovkiy2012@gmail.com>
2020-01-19 22:37:49 +0300

    84 finish

--- src/components/sw-components/person-detalis.js
+++ src/components/sw-components/person-detalis.js
@@ -2,23 +2,19 @@ import React from "react";
 
 import ItemDetails, { Record } from "../item-details/item-details";
 import { SwapiServiceConsumer } from "../swapi-sevice-context/swapi-sevice-context";
-import withSwapiService from '../hoc-helpers/with-swapi-service';
-const PersonDetails = ({ itemId }) => {
+import withSwapiService from "../hoc-helpers/with-swapi-service";
+
+const PersonDetails = ({ itemId, SwapiService }) => {
+  const { getPerson, getPersonImage } = SwapiService;
   return (
-    <SwapiServiceConsumer>
-      {({ getPerson, getPersonImage }) => {
-        return (
-          <ItemDetails
-            itemId={itemId}
-            getData={getPerson}
-            getImageUrl={getPersonImage}
-          >
-            <Record field={"gender"} label={"Gender"} />
-            <Record field={"eyeColor"} label={"Eye color"} />
-          </ItemDetails>
-        );
-      }}
-    </SwapiServiceConsumer>
+    <ItemDetails
+      itemId={itemId}
+      getData={getPerson}
+      getImageUrl={getPersonImage}
+    >
+      <Record field={"gender"} label={"Gender"} />
+      <Record field={"eyeColor"} label={"Eye color"} />
+    </ItemDetails>
   );
 };
-export default withSwapiService (PersonDetails);
+export default withSwapiService(PersonDetails);
